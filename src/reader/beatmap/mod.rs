e4f800015764e7fba18507cc0c57fc4c381a2112
Glubus <50545507+Glubus@users.noreply.github.com>
2025-07-08 11:54:12 +0200

    changed toml

--- src/reader/beatmap/mod.rs
+++ src/reader/beatmap/mod.rs
@@ -8,10 +8,8 @@ use crate::reader::common::OsuType;
 
 
 
-
 pub(crate) fn read_beatmap_info(p: &Process, state: &mut State, osu_type: OsuType) -> eyre::Result<BeatmapInfo>
 {
-
     match osu_type {
         OsuType::Stable => stable::read_beatmap_info(p, state),
         _ => Err(eyre::eyre!("Unsupported osu type for now")),
