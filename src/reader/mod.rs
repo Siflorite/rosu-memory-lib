a6e3936c42670ff76dd777a4656149097015f6a8
Glubus <50545507+Glubus@users.noreply.github.com>
2025-07-08 11:52:25 +0200

    first clean

--- src/reader/mod.rs
+++ src/reader/mod.rs
@@ -9,13 +9,13 @@ use crate::reader::common::GameState;
 use rosu_mem::process::{Process, ProcessTraits};
 use crate::reader::structs::State;
 use crate::reader::structs::StaticAddresses;
-use rosu_mem::process::ProcessError;
+use rosu_mem::error::ProcessError;
 
 mod structs;
 
 static EXCLUDE_WORDS: [&str; 2] = ["umu-run", "waitforexitandrun"];
 
-
+#[allow(dead_code)]
 // Use this function to make callback and get anything you need such as map info or user info or even submit shit
 pub fn waiting_for_play<F>(p: &Process, state: &mut State, callback: Option<F>) -> eyre::Result<()> 
 where 
@@ -31,6 +31,7 @@ where
     }
 }
 
+#[allow(dead_code)]
 pub fn init_loop(sleep_duration: u64) -> eyre::Result<(State, Process)> {
     let mut state = State {
         addresses: StaticAddresses::default(),
@@ -69,58 +70,21 @@ pub fn init_loop(sleep_duration: u64) -> eyre::Result<(State, Process)> {
     Ok((state, p))
 }
 
-pub fn controlla() -> Result<()> {
 
-    let (state, p) = init_loop(300)?;
-
-
-        println!("Starting reading loop");
-        'main_loop: loop {
-            std::thread::sleep(Duration::from_millis(1000));
-            println!("Waiting For Play");
-            if let Err(e) = waiting_for_play(&p, &mut state, None) {
-                match e.downcast_ref::<ProcessError>() {
-                    Some(&ProcessError::ProcessNotFound) => {
-                        continue 'main_loop;
-                    }
-                    #[cfg(target_os = "windows")]
-                    Some(&ProcessError::OsError { .. }) => {
-                        println!("{:?}", e);
-                        continue 'main_loop;
-                    }
-                    Some(_) | None => {
-                        println!("{:?}", e);
-                        continue 'main_loop;
-                    }
-                }
-            }
-            println!("Playing");
-            std::thread::sleep(Duration::from_millis(700));
-            let a = playing(&p, &mut state);
-
-            // let a = wait_result_screen(&p, &mut state) && a;
-    
-    }
-}
-
-pub(crate) fn playing(p: &Process, state: &mut State) -> bool {
-    println!("Playing");
-    let mut cur_time = 0;
-    let mut last_time = 0;
-    let mut last_paused = 0;
-    let mut last_retries = 0;
-    let mode_list = get_mods(p, state);
-    while (GameState::from(get_status(p, state)) == GameState::Playing) {
-        cur_time = reader_gameplay::get_ig_time(p, state);
-        if (cur_time - last_time < 20 && cur_time > 0 && last_time > 0 && last_paused != cur_time) {
-            last_paused = cur_time;
-        }
-        last_time = cur_time;
-        let status = get_status(p, state);
-        let md5 = get_beatmap_md5(p, state);
-        if (last_retries < get_retries(p, state)) {
-            return false;
-        }
-    }
-    true
-}
+// Exemple of playing loop
+// pub(crate) fn playing(p: &Process, state: &mut State) -> bool {
+//     let mode_list = get_mods(p, state);
+//     while (GameState::from(get_status(p, state)) == GameState::Playing) {
+//         cur_time = reader_gameplay::get_ig_time(p, state);
+//         if (cur_time - last_time < 20 && cur_time > 0 && last_time > 0 && last_paused != cur_time) {
+//             last_paused = cur_time;
+//         }
+//         last_time = cur_time;
+//         let status = get_status(p, state);
+//         let md5 = get_beatmap_md5(p, state);
+//         if (last_retries < get_retries(p, state)) {
+//             return false;
+//         }
+//     }
+//     true
+// }
